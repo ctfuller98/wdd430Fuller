af2bce3e3d2cd128d6b262118efb3a989475d6ec
ctfuller98 <ful17005@byui.edu>
2022-05-13 13:56:32 -0600

    week 4 work, documents

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'app-document-list',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent implements OnInit {
-
+  @Output()  selectedDocumentEvent = new EventEmitter<Document>();
+  documents:Document[] = [
+    new Document(1, "Test File 1", "First Test File", "url.org", null),
+    new Document(2, "Test File 2", "Second Test File", "url.org", null),
+    new Document(3, "Test File 3", "Third Test File", "url.org", null),
+    new Document(4, "Test File 4", "Fourth Test File", "url.org", null),
+    new Document(5, "Test File 5", "Fifth Test File", "url.org", null)
+  ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onSelectedDocument(document:Document){
+    this.selectedDocumentEvent.emit(document)
   }
 
 }
