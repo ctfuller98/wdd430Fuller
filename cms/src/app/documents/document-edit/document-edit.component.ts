79358a1fa0cafb4a99300ee38669313dd3607ab3
ctfuller98 <ful17005@byui.edu>
2022-06-17 15:23:51 -0600

    document component

--- cms/src/app/documents/document-edit/document-edit.component.ts
+++ cms/src/app/documents/document-edit/document-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Document } from '../document.model';
+import { NgForm} from '@angular/forms';
+import { DocumentService } from '../document.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-document-edit',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-edit.component.css']
 })
 export class DocumentEditComponent implements OnInit {
-
-  constructor() { }
+  originalDocument: Document
+  document: Document
+  editMode: boolean = false
+  id: string;
+  constructor(private documentService: DocumentService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params
+    .subscribe(
+      (params: Params)=> {
+        this.id = params['id']
+        if(this.id===null || this.id==undefined){
+          this.editMode = false;
+          return
+        }
+        this.originalDocument = this.documentService.getDocument(this.id)
+        if(this.originalDocument == undefined || this.originalDocument==null){
+            return}
+        this.editMode = true
+        this.document = JSON.parse(JSON.stringify(this.originalDocument));
+        
+      })
+    } 
+  
+  onCancel() {
+    this.router.navigate(['/documents'])
   }
-
+  onSubmit(form: NgForm){
+      const value = form.value
+      const newDocument = new Document(this.id, value.name, value.description, value.url, null); 
+      if (this.editMode === true){
+        this.documentService.updateDocument(this.originalDocument, newDocument)
+      } 
+      else {
+        this.documentService.addDocument(newDocument)
+      }
+      this.router.navigate(['/documents'])
+  }
 }
--- cms/src/app/documents/document.service.ts
+++ cms/src/app/documents/document.service.ts
@@ -1,19 +1,25 @@
-import { EventEmitter, Injectable } from '@angular/core';
+import {Injectable } from '@angular/core';
 import { Document } from './document.model';
-import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+import { Subject } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentService {
 documents: Document[] = [];
-documentChangedEvent = new EventEmitter<Document[]>();
-  constructor() { 
-    this.documents = MOCKDOCUMENTS;
+documentListChangedEvent = new Subject<Document[]>();
+maxId: number;
+currentId: number;
+maxDocumentId: number;
+  constructor(private httpClient: HttpClient) { 
+    this.getDocuments();
+    this.maxDocumentId = this.getMaxId();
   }
+  
   getDocuments(): Document[]{
-    let list = this.documents.sort();
-    return list.slice();
+     this.fetchDocuments()
+     return
   }
   getDocument(id: string): Document {
     for (let i = 0; i < this.documents.length; i++) {
@@ -23,6 +29,16 @@ documentChangedEvent = new EventEmitter<Document[]>();
     }
     return null;
   } 
+  getMaxId(): number {
+    this.maxId = 0;
+    this.documents.forEach(document => {
+      this.currentId = parseFloat(document.id)
+      if (this.currentId > this.maxId){
+        this.maxId = this.currentId;
+      }
+    });
+    return this.maxId
+}
   deleteDocument(document: Document) {
     if (!document) {
        return;
@@ -32,7 +48,55 @@ documentChangedEvent = new EventEmitter<Document[]>();
        return;
     }
     this.documents.splice(pos, 1);
-    this.documentChangedEvent.emit(this.documents.slice());
+    this.storeDocuments()
  }
-  
+ 
+addDocument(newDocument: Document) {
+  if(newDocument == undefined || newDocument == null){
+    console.log("undefined document")
+    return
+  }
+  this.maxDocumentId++;
+  newDocument.id = this.maxDocumentId.toString();
+  this.documents.push(newDocument)
+  this.storeDocuments()
+}
+
+updateDocument(originalDocument: Document, newDocument: Document) {
+  if(!originalDocument || !newDocument|| originalDocument === null || newDocument === null) {
+    return
+  }
+  let pos = this.documents.indexOf(originalDocument);
+  if (pos < 0) {
+    return
+  }
+  newDocument.id = originalDocument.id
+  this.documents[pos] = newDocument
+  this.storeDocuments();
+}
+private fetchDocuments(){
+  this.httpClient.get('https://wdd430cms-e579c-default-rtdb.firebaseio.com/documents.json')
+  .subscribe (
+    (documents:Document[]) => {
+      this.documents = documents
+      this.maxDocumentId = this.getMaxId()
+      let list = this.documents.sort();
+      this.documentListChangedEvent.next(list)
+    },
+    (error:any) => {
+      console.log(error)
+      }
+    )
+  }
+storeDocuments() {
+  const docList = JSON.stringify(this.documents);
+  this.httpClient.put('https://wdd430cms-e579c-default-rtdb.firebaseio.com/documents.json', docList,
+  {
+    headers: new HttpHeaders({"Content-Type" : "application/json"})
+  })
+  .subscribe(response=>{
+    let documentsListClone = this.documents.slice()
+    this.documentListChangedEvent.next(documentsListClone)
+  })
+}
 }
